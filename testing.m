97bab14c046565365f40335123cfd03cd9877871
Bukhari4 <37036077+Bukhari4@users.noreply.github.com>
2018-03-04 02:40:16 -0800

    Add files via upload

--- testing.m
+++ testing.m
@@ -1,5 +1,14 @@
-imgTestSets = imageSet('C:\Users\Bukhari\Documents\MATLAB\EE146\Project\try3\TESSTING','recursive')
-[labelIdx, scores] = predict(categoryClassifier, imgTestSets);
+% imgTestSets = imageSet('C:\Users\Bukhari\Documents\MATLAB\EE146\Project\try3\TESSTING','recursive')
+% [labelIdx, scores] = predict(categoryClassifier, imgTestSets);
 
+%cam = webcam('Lenovo EasyCamera')
+I = snapshot(cam);
+[labelIdx, scores] = predict(categoryClassifier, I);
+imshow(I)
 % Display the string label
-categoryClassifier.Labels(labelIdx)
+s=categoryClassifier.Labels(labelIdx)
+text = char(s);
+NET.addAssembly('System.Speech');
+obj = System.Speech.Synthesis.SpeechSynthesizer;
+obj.Volume = 100;
+Speak(obj, text);
