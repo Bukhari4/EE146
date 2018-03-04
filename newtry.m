8d0fd67e22cfa677736d395db9aa163de9192b59
Bukhari4 <37036077+Bukhari4@users.noreply.github.com>
2018-03-03 23:52:40 -0800

    Add files via upload

---
+++ newtry.m
@@ -0,0 +1,32 @@
+clear all
+close all
+clc
+imgSets = imageSet('C:\Users\Bukhari\Documents\MATLAB\EE146\Project\try3\SAMPLES','recursive');
+{ imgSets.Description } % display all labels on one line
+[imgSets.Count]         % show the corresponding count of images
+minSetCount = min([imgSets.Count]); % determine the smallest amount of images in a category
+
+% Use partition method to trim the set.
+imgSets = partition(imgSets, minSetCount, 'randomize');
+
+% Notice that each set now has exactly the same number of images.
+[imgSets.Count]
+[trainingSets, validationSets] = partition(imgSets, 0.3, 'randomize');
+bag = bagOfFeatures(trainingSets, 'VocabularySize',2000)%,'Upright',false);
+img = read(imgSets(1), 1);
+featureVector = encode(bag, img);
+
+% Plot the histogram of visual word occurrences
+figure
+bar(featureVector)
+title('Visual word occurrences')
+xlabel('Visual word index')
+ylabel('Frequency of occurrence')
+categoryClassifier = trainImageCategoryClassifier(trainingSets, bag);
+
+confMatrix = evaluate(categoryClassifier, trainingSets);
+
+confMatrix = evaluate(categoryClassifier, validationSets);
+
+% Compute average accuracy
+mean(diag(confMatrix));
---
+++ testing.m
@@ -0,0 +1,6 @@
+imgTestSets = imageSet('C:\Users\Bukhari\Documents\MATLAB\EE146\Project\try3\TESSTING','recursive');
+
+[labelIdx, scores] = predict(categoryClassifier, imgTestSets);
+
+% Display the string label
+categoryClassifier.Labels(labelIdx)
