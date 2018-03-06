40efd34e554dde3bb8043b8318b299ff6db31abe
Allen Hsu <bboypupu@gmail.com>
2018-03-06 14:26:18 -0800

    Procdure of foreground extraction: 1) blur the image with Gaussian filter. 2) get the ROI matrix using k-means clustering with k = 3, and repeat for 3 times to avoid local minimum 3) morphologically close the ROI matrix to avoid holes on the image 4) mask the original image with ROI matrix. (added some test images)

--- foreground_extraction.m
+++ foreground_extraction.m
@@ -2,7 +2,7 @@ clear all
 close all
 clc
 
-I = imread('./TESSTING/apple.jpg');
+I = imread('./TESSTING/tomato-2.jpg');
 % I_g = rgb2gray(I);
 % th = graythresh(I_g);
 % mask = imbinarize(I_g,th);
@@ -17,7 +17,7 @@ nrows = size(ab,1);
 ncols = size(ab,2);
 ab = reshape(ab,nrows*ncols,2);
 
-nColors = 2;
+nColors = 3;
 % repeat the clustering 3 times to avoid local minima
 [cluster_idx, cluster_center] = kmeans(ab,nColors,'distance','sqEuclidean', ...
                                       'Replicates',3);
@@ -25,15 +25,16 @@ pixel_labels = reshape(cluster_idx,nrows,ncols);
 % figure, imshow(I);
 figure, imshow(pixel_labels,[]), title('image labeled by cluster index');
 target = pixel_labels(int32(nrows/2), int32(ncols/2));
-pixel_labels = pixel_labels - 1;
 M = zeros(nrows, ncols);
 for i = 1:nrows
     for j = 1:ncols
-        if pixel_labels(i,j) ~= target
+        if pixel_labels(i,j) == target
             M(i,j) = 1;
         end
     end
 end
+se = strel('disk',10);
+M = imclose(M,se);
 masked_rgb_image = bsxfun(@times, I, cast(M, 'like', I));
-% masked_rgb_image = I.*repmat(M,[1,1,3]);
 figure, imshow(masked_rgb_image);
+imwrite(masked_rgb_image, 'bs_image.jpg');
