1b1816a76a405ed5013118d365478c3c15c48bc0
Allen Hsu <bboypupu@gmail.com>
2018-03-06 02:51:28 -0800

    foreground extraction via k-means clustering

--- test_cam.m
+++ test_cam.m
@@ -4,15 +4,14 @@ DC = 'img_from_cam';
 if(exist(DC,'dir')~=7)
     mkdir(DC);
 end
-cnt = 1;
-while(true)
+
+for idx = 1:10
     img = snapshot(cam);
-    fname = sprintf('%04d.jpg',cnt);
+    fname = sprintf('%04d.jpg',idx);
     ffn = fullfile(DC,fname);
-    cnt = cnt+1;  
-%     imwrite(data,ffn);
+    imwrite(img,ffn);
     imshow(img);
     pause(1.0);
 end
 
-% clear cam;
+clear cam;
